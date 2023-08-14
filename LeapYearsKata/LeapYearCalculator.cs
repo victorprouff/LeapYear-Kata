7af256c22ef936396981d542a7e2c7d2bf0881d7
Victor Prouff <contact@victorprouff.fr>
2023-08-14 12:08:44 +0200

    Extract IsDivisibleBy100

--- LeapYearsKata/LeapYearCalculator.cs
+++ LeapYearsKata/LeapYearCalculator.cs
@@ -4,13 +4,15 @@ public class LeapYearCalculator
 {
     public static bool IsLeapYear(uint year)
     {
-        if (IsDivisibleBy4(year) && (year % 100 != 0))
+        if (IsDivisibleBy4(year) && IsDivisibleBy100(year) is false)
         {
             return true;
         }
 
         return false;
     }
+
+    private static bool IsDivisibleBy100(uint year) => (year % 100 == 0);
 
     private static bool IsDivisibleBy4(uint year) => (year % 4 == 0);
 }
