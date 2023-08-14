4ad2c781148594fc32d1a120dfd6566a0e30cd1b
Victor Prouff <contact@victorprouff.fr>
2023-08-14 12:23:29 +0200

    Extract IsAtypicalLeap method

--- LeapYearsKata/Year.cs
+++ LeapYearsKata/Year.cs
@@ -4,8 +4,10 @@ public static class Year
 {
     public static bool IsLeap(uint year)
     {
-        return year.IsDivisibleBy(4) && (year.IsDivisibleBy(100) is false || year.IsDivisibleBy(400));
+        return year.IsDivisibleBy(4) && IsAtypicalLeap(year);
     }
+
+    private static bool IsAtypicalLeap(uint year) => year.IsDivisibleBy(100) is false || year.IsDivisibleBy(400);
 
     private static bool IsDivisibleBy(this uint year, uint modulo) => year % modulo == 0;
 }
