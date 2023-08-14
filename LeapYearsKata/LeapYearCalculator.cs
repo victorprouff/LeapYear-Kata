2b7ec724d5eab7ae111cab5df038cba8475c5d11
Victor Prouff <contact@victorprouff.fr>
2023-08-14 12:16:08 +0200

    Add Atypical Values on tests

--- LeapYearsKata/LeapYearCalculator.cs
+++ LeapYearsKata/LeapYearCalculator.cs
@@ -4,15 +4,10 @@ public class LeapYearCalculator
 {
     public static bool IsLeapYear(uint year)
     {
-        if (IsDivisibleBy4(year) && IsDivisibleBy100(year) is false)
-        {
-            return true;
-        }
-
-        return false;
+        return IsDivisibleBy4(year) && (IsDivisibleBy100(year) is false || IsDivisibleBy400(year));
     }
 
+    private static bool IsDivisibleBy400(uint year) => (year % 400 == 0);
     private static bool IsDivisibleBy100(uint year) => (year % 100 == 0);
-
     private static bool IsDivisibleBy4(uint year) => (year % 4 == 0);
 }
--- LeapYearsKataTests/LeapYearShould.cs
+++ LeapYearsKataTests/LeapYearShould.cs
@@ -10,6 +10,8 @@ public class LeapYearShould
     [InlineData(1808)]
     [InlineData(1812)]
     [InlineData(1816)]
+    [InlineData(1996)]
+    [InlineData(2000)]
     public void ReturnTrueWhenYearIsLeapYear(uint year)
     {
         var result = LeapYearCalculator.IsLeapYear(year);
@@ -24,6 +26,7 @@ public class LeapYearShould
     [InlineData(1809)]
     [InlineData(1800)]
     [InlineData(1900)]
+    [InlineData(2001)]
     public void ReturnFalseWhenYearIsNotLeapYear(uint year)
     {
         var result = LeapYearCalculator.IsLeapYear(year);
