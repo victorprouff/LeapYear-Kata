500146eeeb5ff30da7d43308c85b6e3078e5f21f
Victor Prouff <contact@victorprouff.fr>
2023-08-14 12:02:55 +0200

    Implement rule of year % 100 != 0 for leapyear

--- LeapYearsKata/LeapYearCalculator.cs
+++ LeapYearsKata/LeapYearCalculator.cs
@@ -4,6 +4,11 @@ public class LeapYearCalculator
 {
     public static bool IsLeapYear(uint year)
     {
-        return year % 4 == 0;
+        if ((year % 4 == 0) && (year % 100 != 0))
+        {
+            return true;
+        }
+
+        return false;
     }
 }
